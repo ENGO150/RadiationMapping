054309857da4d3b80679cab08b0e4ee70aee5c02
ENGO150 <v.smejkal06@gmail.com>
2024-11-14 20:42:50 +0100

    added licenses to shell files

--- setup.sh
+++ setup.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
+# This is part of RadiationMapping
+# Copyright (C) 2024 Václav Šmejkal
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 arduino-cli core update-index
 arduino-cli core install --additional-urls http://arduino.esp8266.com/stable/package_esp8266com_index.json esp8266:ESP8266
