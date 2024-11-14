6b43aa44fabdf01bee1f47d7b84e7aa65a2b2d81
ENGO150 <v.smejkal06@gmail.com>
2024-11-14 18:41:47 +0100

    added license to top of main file

--- RadiationMapper.ino
+++ RadiationMapper.ino
@@ -1,3 +1,21 @@
+/*
+This is part of RadiationMapping
+Copyright (C) 2022 Václav Šmejkal
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <list>
 
 #define INPUT_PIN D6 //VIN PIN FOR GEIGER COUNTER
